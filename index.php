e4e2cf10cc9f0b9566e397ee4cd4d41c0afd4035
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-03-03 13:06:27 +0100

    go with silex

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function() use ($app) {
+    return 'hello world';
+});
+
+$app->get('/generatekey', function() use ($app) {
+    return 'key';
+});
+
+$app->run();
