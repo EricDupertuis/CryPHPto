249749451ab9d476c881106d2479816b3adcf0ad
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-05-31 21:57:14 +0200

    work in progress

---
+++ app/config/routing.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eric
+ * Date: 31/05/16
+ * Time: 12:54
+ */
---
+++ src/EricDupertuis/Cryphpto/App.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eric
+ * Date: 31/05/16
+ * Time: 12:55
+ */
+
+namespace EricDupertuis\Cryphpto;
+
+
+class App
+{
+
+}
